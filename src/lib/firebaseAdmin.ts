fe98dc013cb498db72889ea100950f21b93711bd
pabitra18-dev <pabitra14civil25@kcc.edu.np>
2026-08-03 01:27:58 +0545

    Initialize Firestore and export db instance
    Added Firestore Admin instance and updated exports.

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,15 +1,18 @@
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  // Parse the service account details from an environment variable string
   const serviceAccount = JSON.parse(
     process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}'
   );
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    // databaseURL: "https://your-database-name.firebaseio.com" // include if needed
   });
 }
 
+// Initialize the Firestore Admin instance
+const db = admin.firestore();
+
+// Export both the main admin instance and the db instance
+export { db };
 export default admin;
